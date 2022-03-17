9b097e54af0e1091d45332f6d56577b1cadd7b3f
Jacob D. Castro <jacob@jdc.dev>
2022-03-17 16:38:32 -0700

    fix broken link

--- client/src/components/AppLayout/Sidebar.tsx
+++ client/src/components/AppLayout/Sidebar.tsx
@@ -2,12 +2,13 @@
 import axios from 'axios';
 import React from 'react';
 import { useQuery } from 'react-query';
+import { Link } from 'react-router-dom';
 import { useAuth } from '../../hooks/useAuth';
 import { setApiUrl } from '../../lib/apiUrl';
 
 type Props = { daoId: string; profile: any };
 
-const DaoItem = ({ daoId, profile, selectDao }: Props) => {
+const DaoItem = ({ daoId, profile }: Props) => {
   const { data: dao } = useQuery([daoId, profile], async () => {
     if (daoId) {
       const { data } = await axios({
@@ -19,28 +20,22 @@ const DaoItem = ({ daoId, profile, selectDao }: Props) => {
   });
 
   return (
-    <button
-      className='bg-blue-500 w-11 h-11 rounded-full mb-2'
-      onClick={() => selectDao(daoId.id)}
-    >
-      {dao && <img src={dao.image} className='rounded-full' />}
-    </button>
+    <Link to={`/daos/${daoId}`}>
+      <button className='bg-blue-500 w-11 h-11 rounded-full mb-2'>
+        {dao && <img src={dao.image} className='rounded-full' />}
+      </button>
+    </Link>
   );
 };
 
 const Sidebar = () => {
-  const { profile, selectDao } = useAuth();
+  const { profile } = useAuth();
   const daos = profile ? profile?.memberships : [];
 
   return (
     <div className='bg-white flex flex-col w-16 h-screen py-3 items-center border-r-2 border-r-slate-100'>
       {daos.map(daoId => (
-        <DaoItem
-          key={daoId}
-          daoId={daoId}
-          profile={profile}
-          selectDao={selectDao}
-        />
+        <DaoItem key={daoId} daoId={daoId} profile={profile} />
       ))}
     </div>
   );
